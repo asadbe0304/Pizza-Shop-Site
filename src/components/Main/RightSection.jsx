56f1558a4c1cead06a52193848fb2905f8439a94
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-09-15 23:13:14 +0500

    category corusel add

--- src/components/Main/RightSection.jsx
+++ src/components/Main/RightSection.jsx
@@ -1,11 +1,13 @@
 import Img from "./../../../public/images/pizzaa2.png";
 import Img3 from "./../../../public/images/fris.png";
+import Img4 from "./../../assets/herobanner.png";
 const RightSection = () => {
   return (
     <>
       <div className="hero__right ">
-        <img src={Img} width={"450px"} height={"683px"} alt="hero pizza images" />
+        <img src={Img} width={"450px"} height={"683px"} className="desktop-img" alt="hero pizza images" />
         <img src={Img3} width={"212px"} className="fries" height={"226px"} alt="hero fries images" />
+        <img src={Img4} width={"212px"} className="mobile-img" height={"226px"} alt="hero fries images" />
       </div>
     </>
   );
--- src/components/Menu/Category.jsx
+++ src/components/Menu/Category.jsx
@@ -1,45 +1,161 @@
 import Btn from "./../UI/OutButton";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { Component } from "react";
 
-const Category = () => {
-  const db = [
-    {
-      category: "All",
-    },
-    {
-      category: "Combo",
-    },
-    {
-      category: "Burger",
-    },
-    {
-      category: "Spinner",
-    },
-    {
-      category: "Chicken",
-    },
-    {
-      category: "Pizza",
-    },
-    {
-      category: "Drinks",
-    },
-    {
-      category: "Deserts",
-    },
-    {
-      category: "Salads",
-    },
-  ];
-  const style = "bg-transparent ";
-  return (
-    <>
-      <div className="flex items-center snap-mandatory gap-3 overflow-x-auto overflow-y-hidden">
-        {db.map((e) => {
-          return <Btn text={e.category} style={style} key={e.category} />;
-        })}
+// import Slider from "react-slick";
+// const Category = () => {
+//   const style = "bg-transparent ";
+//   const settings = {
+//     infinite: true,
+//     centerPadding: "60px",
+//     slidesToShow: 6,
+//     swipeToSlide: true,
+//   };
+
+//   return (
+//     <>
+//       <div className="flex items-center">
+//         <Slider className="overflow-hidden" {...settings}>
+//           {db.map((e) => {
+//             return <Btn text={e.category} style={style} key={e.category} />;
+//           })}
+//         </Slider>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Category;
+
+import Slider from "react-slick";
+
+export default class PreviousNextMethods extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
+  render() {
+    const db = [
+      {
+        category: "All",
+      },
+      {
+        category: "Combo",
+      },
+      {
+        category: "Burger",
+      },
+      {
+        category: "Spinner",
+      },
+      {
+        category: "Chicken",
+      },
+      {
+        category: "Pizza",
+      },
+      {
+        category: "Drinks",
+      },
+      {
+        category: "Deserts",
+      },
+      {
+        category: "Salads",
+      },
+    ];
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      centerMode: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      spaceBetween: 10,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+          },
+        },
+        {
+          breakpoint: 840,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 540,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 490,
+          settings: {
+            centerMode:true,
+            slidesToShow: 1.5,
+          },
+        },
+        {
+          breakpoint: 390,
+          settings: {
+            centerMode:true,
+            slidesToShow: 1.3,
+          },
+        },
+        {
+          breakpoint: 350,
+          settings: {
+            centerMode:true,
+            slidesToShow: 1.1,
+          },
+        },
+        {
+          breakpoint: 320,
+          settings: {
+            centerMode:true,
+            slidesToShow: 1,
+          },
+        },
+      ],
+    };
+    return (
+      <div className="relative">
+        <Slider ref={(c) => (this.slider = c)} {...settings}>
+          {db.map((e) => {
+            return <Btn text={e.category} key={e.category} />;
+          })}
+        </Slider>
+        {/* <div style={{ textAlign: "center" }}> */}
+        <div
+          className="bg-white cursor-pointer rounded-full top-0 w-8 h-8 absolute"
+          onClick={this.previous}
+        >
+          <GrFormPrevious className="w-8 h-8" style={{ translate: "180deg" }} />
+        </div>
+        <div
+          className="bg-white rounded-full cursor-pointer w-8 h-8 top-0 right-0 absolute"
+          onClick={this.next}
+        >
+          <GrFormNext className="w-8 h-8" />
+        </div>
+        {/* </div> */}
       </div>
-    </>
-  );
-};
-
-export default Category;
+    );
+  }
+}
