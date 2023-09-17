2c4fc5c2694bc698c49c224970af85b75eaccf61
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-09-17 06:33:09 +0500

    ok

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,8 @@
 const index = () => {
   return (
     <>
-      <section>
+      <section className="mt-16 2xl:mt-10 lg:mt-10 ">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-         
           <div className="w-full rounded-lg shadow shadow-white md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
