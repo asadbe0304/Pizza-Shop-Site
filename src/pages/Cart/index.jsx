39fcd8caf5af9fc5655744e68be86d80a1b0c051
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-09-18 19:24:51 +0500

    Ok

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,6 +1,7 @@
 import "./index.scss";
 import { MdClose } from "react-icons/md";
 import { GiTrashCan } from "react-icons/gi";
+import Img from "./../../assets/piz.png"
 import BTN from "./../../components/UI/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { modalCartClose } from "../../slice/auth";
@@ -17,14 +18,14 @@ const index = () => {
           cart ? "flex animating" : "hidden"
         }`}
       >
-        <div className="icon-close absolute bg-red-600 hover:bg-red-400 hover:scale-95 text-white rounded-full p-1 border top-5 right-5">
-          <MdClose onClick={hide} className="w-8 h-8 cursor-pointer" />
+        <div className="icon-close absolute cart-close hover:bg-red-400  hover:scale-95 text-white rounded-full p-1  top-5 right-5">
+          <MdClose onClick={hide} className="w-8 h-8  cursor-pointer" />
         </div>
-        <div className="cart p-6 flex flex-col gap-3 items-center justify-between bg-black  h-full pt-10 xl:w-5/12 lg:w-8/12 md:w-8/12 sm:w-9/12 w-full rounded-xl border">
+        <div className="cart p-6 flex flex-col gap-3 items-center justify-between bg-black  h-full pt-10 xl:w-5/12 lg:w-8/12 md:w-8/12 sm:w-9/12 w-full rounded-xl ">
           <h3 className="text-white">Orders</h3>
           <ul className="cart-list w-full flex-col gap-4 p-1 bg-zinc-400 rounded-xl justify-between flex items-start ">
-            <li className="flex items-center justify-between p-3 bg-slate-500 border rounded-md w-full">
-              <img src="" className="w-10 h-10" alt="img" />
+            <li className="flex items-center cart-close justify-between p-3 bg-slate-500  rounded-md w-full">
+              <img src={Img} className="w-10 h-10" alt="img" />
               <div className="flex flex-col items-start gap-3">
                 <h3 className="text-white">Burger</h3>
                 <strong className="text-white flex items-center justify-between gap-1">
@@ -44,8 +45,8 @@ const index = () => {
                 </div>
               </div>
             </li>
-            <li className="flex items-center justify-between p-3 bg-slate-500 border rounded-md w-full">
-              <img src="" className="w-10 h-10" alt="img" />
+            <li className="flex items-center justify-between cart-close p-3 bg-slate-500  rounded-md w-full">
+              <img src={Img} className="w-10 h-10" alt="img" />
               <div className="flex flex-col items-start gap-3">
                 <h3 className="text-white">Burger</h3>
                 <strong className="text-white flex items-center justify-between gap-1">
@@ -65,8 +66,8 @@ const index = () => {
                 </div>
               </div>
             </li>
-            <li className="flex items-center justify-between p-3 bg-slate-500 border rounded-md w-full">
-              <img src="" className="w-10 h-10" alt="img" />
+            <li className="flex items-center justify-between cart-close p-3 bg-slate-500  rounded-md w-full">
+              <img src={Img} className="w-10 h-10" alt="img" />
               <div className="flex flex-col items-start gap-3">
                 <h3 className="text-white">Burger</h3>
                 <strong className="text-white flex items-center justify-between gap-1">
@@ -91,7 +92,7 @@ const index = () => {
             <div className="text-white flex w-full my-4 items-center justify-between">
               Total amount : <strong>34 000 uzs</strong>
             </div>
-            <div className="border-t-2 w-full pt-2 flex items-center justify-center">
+            <div className="-t-2 w-full pt-2 flex items-center justify-center">
               <BTN text={"Shopping Submit"} />
             </div>
           </div>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -12,7 +12,7 @@ const index = () => {
                 <div>
                   <label
                     htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-white dark:text-white"
+                    className="block mb-2 text-sm font-medium text-white  dark:text-white"
                   >
                     Your email
                   </label>
@@ -20,7 +20,7 @@ const index = () => {
                     type="email"
                     name="email"
                     id="email"
-                    className="bg-gray-50 border border-gray-300 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                     required=""
                   />
@@ -37,7 +37,7 @@ const index = () => {
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-30 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
                 </div>
